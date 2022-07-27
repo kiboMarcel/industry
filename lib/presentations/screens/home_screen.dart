d3b9745a59cc9a7cc381a54e9a9e89781e9cefab
nouletame <nouletamemarcel@gmail.com>
2022-07-27 14:14:30 +0000

    search_scrollPosition

--- lib/presentations/screens/home_screen.dart
+++ lib/presentations/screens/home_screen.dart
@@ -1,48 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:industry/utils/dimensions.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late ScrollController _scrollController;
+
+  @override
+  void initState() {
+    _scrollController = ScrollController();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
         centerTitle: true,
-        title: Text('App Name'),
+        title: InkWell(
+          onTap: () => _scrollController.jumpTo(0),
+          child: Text(
+            'App Name',
+            style: TextStyle(color: Colors.black45),
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              showSearch(context: context, delegate: MySearchDelegate());
+            },
+            icon: Icon(
+              Icons.search_rounded,
+              size: Dimensions.iconseSize24,
+              color: Colors.black45,
+            ),
+          ),
+        ],
       ),
       body: Column(
         children: [
           SizedBox(
-            height: 20,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Text(
-                'LISTE DES\nMACHINES',
-                style: TextStyle(
-                  fontSize: Dimensions.font26 + 5,
-                ),
-              ),
-              Icon(
-                Icons.search_rounded,
-                size: Dimensions.iconseSize24 + 20,
-              ),
-            ],
+            height: Dimensions.height10 - 5,
           ),
           SizedBox(
             height: Dimensions.height15,
           ),
           Expanded(
             child: ListView.builder(
+                controller: _scrollController,
                 itemCount: 20,
                 itemBuilder: (context, index) {
                   return Padding(
-                    padding: const EdgeInsets.all(8.0),
+                    padding: const EdgeInsets.all(3.0),
                     child: ListTile(
-                      leading: Icon(Icons.mail_lock_rounded),
-                      title: Text('Hello'),
+                      onTap: () {},
+                      leading: Icon(Icons.build),
+                      title: Text('Machine ${index + 1} '),
                     ),
                   );
                 }),
@@ -52,8 +72,6 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
-
-
 
 /* Container _buildGradient(Animation<double> animation, context) {
   return Container(
@@ -72,3 +90,36 @@ class HomeScreen extends StatelessWidget {
     ),
   );
 } */
+
+class MySearchDelegate extends SearchDelegate {
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+        onPressed: () => close(context, null), icon: Icon(Icons.arrow_back));
+  }
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+          onPressed: () {
+            if (query.isEmpty) {
+              close(context, null);
+            } else {
+              query = '';
+            }
+          },
+          icon: Icon(Icons.clear)),
+    ];
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Container();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return Container();
+  }
+}
